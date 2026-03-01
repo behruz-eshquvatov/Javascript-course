3ae382a838dbff3e952e2968a582a17440003549
behruz-eshquvatov <113596209+behruz-eshquvatov@users.noreply.github.com>
2026-03-01 15:14:43 +0500

    2-dars yuklandi

---
+++ 2-dars/script.js
@@ -0,0 +1,13 @@
+// comments => izohlar
+
+// CRTL + / => single line comment
+
+// Comment — bu kod ichidagi tushuntirish yozuvi.
+// Brauzer uni o‘qimaydi, faqat dasturchi uchun yoziladi.
+
+
+// single line => bir qator 
+
+/* 
+Multi line comment=> ko'p qator comment
+*/
