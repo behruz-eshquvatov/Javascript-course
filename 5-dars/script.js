9d6b54b38a349788ac05a19810a529c5d07910ba
behruz-eshquvatov <113596209+behruz-eshquvatov@users.noreply.github.com>
2026-03-02 23:28:38 +0500

    5-dars yuklandi

---
+++ 5-dars/script.js
@@ -0,0 +1,27 @@
+// 1️⃣ O'zgaruvchi nomi harf, _ yoki $ bilan boshlanishi kerak
+let yosh = 18
+let _yosh = 18
+let $yosh = 18
+// 2️⃣ O'zgaruvchi nomi raqam bilan boshlanmaydi
+// let 2006 = 12 => xato
+
+// 3️⃣ Nom ichida bo'sh joy (space) ishlatib bo'lmaydi
+// let user name = 18 => xato
+
+// 4️⃣ Maxsus belgilar (!, @, #, -, va boshqalar) ishlatib bo'lmaydi
+// let user!name = 18 => xato
+
+// 5️⃣ JavaScript band (reserved) so'zlarini nom sifatida ishlatib bo'lmaydi
+// let let = 16 => xato
+
+// 6️⃣ Odatda camelCase yozish usuli ishlatiladi
+let menUzbekistondaYashayman =  12
+const iLoveUzbekistan = 12
+
+// 7️⃣ Nom ma'noli va tushunarli bo'lishi kerak
+
+// 8️⃣ O'zgaruvchi nomlari katta-kichik harfga sezgir (case-sensitive)
+
+let age = 12
+let Age = 12
+let AGe = 12
