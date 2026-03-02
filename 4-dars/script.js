595d97505881d21ed60663ab14d7958889e208d3
behruz-eshquvatov <113596209+behruz-eshquvatov@users.noreply.github.com>
2026-03-02 14:38:05 +0500

    4-dars yakunlandi

---
+++ 4-dars/script.js
@@ -0,0 +1,51 @@
+// ========================= VARIABLES =========================
+
+// Variables => O'zgaruvchilar => Malumot saqlash uchun qutilar.
+
+
+
+// let, const, var,
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let => malumot uzgaradi
+let yosh = 18
+yosh = 16
+console.log(yosh);
+yosh = 8
+console.log(yosh)
+
+
+// const -> uzgarmas, malumot uzgarmaydi
+const PI = 3.14
+console.log(PI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var -> let'ga uxshab ishlaydi, lekin eskiroq versiyasi 
+var sana = 1
+sana = 2
+
+console.log(sana);
