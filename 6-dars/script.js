1840d717d85c697758ccce452ef5bea4f6007f55
behruz-eshquvatov <113596209+behruz-eshquvatov@users.noreply.github.com>
2026-03-03 00:29:10 +0500

    6-dars yuklandi

---
+++ 6-dars/script.js
@@ -0,0 +1,48 @@
+// =================== DATA TYPES ========================
+// ================ MALUMOT TURLARI ======================
+
+
+// Primitive/Sodda => number, string, boolean, NaN, Undefined, Null
+// Reference => object, array, functions
+
+
+// Primitive => yozuvlar, raqamlar, ha yoki yoq ...
+
+// 1. number
+let primitiveNumber = 12
+
+// 2. string => "", '', `` ichidagi hamma narsa
+let primitiveString = "uzbekiston"
+primitiveString = "12"
+primitiveString = 'uzbekistan123!'
+primitiveString = `TABning ustidagi tugma`
+
+// 3. boolean => True/False
+let qizbolami = false
+
+// 4. NaN => not a number
+let bolish = 12/"salom" // NaN
+
+// 5. Null => 0
+let nol = null
+
+// 6. undefined => qiymag topilmadi. qachonki bizning o'zgaruvchimiz bo'sh.
+let topilmadi = undefined
+console.log(topilmadi);
+
+
+// Reference => collection of primitive data. 
+// Sodda malumotlarda yig'ilgan to'plam.
+// Misol: ID carta => sana, ism, yosh, yashash manzil
+
+// 7. Object
+let idCard = {
+   ism: "Behruz",
+   yosh: 22,
+   jinsi: "O'g'il bola"
+}
+
+// 8. Arrays
+let raqamlar = [1, 2, 3, 4, 12, 1, 123]
+
+// 9. Functions
